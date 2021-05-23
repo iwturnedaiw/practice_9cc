b8219db8274f26a83bab8532d1540c2bfa209d4d
Shohei Minami <minami.s.ae@m.titech.ac.jp>
2021-05-23 10:25:20 +0900

    昔の関数定義を試す

---
+++ s_c1-7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int power(int, int);
+
+int main() {
+  return power(2, 4);
+}
+
+int power(base, n)
+int base, n;
+{
+  int i, p;
+
+  p = 1;
+  for ( i = 0; i < n; ++i ) p *= base;
+
+  return p;
+}
